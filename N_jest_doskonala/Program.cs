0d662a353a4347d7a24d068365f62def54ea20b9
tomekzpolski1 <tomasz.rozynski92@gmail.com>
2022-03-15 08:48:47 +0100

    Add project files.

---
+++ N_jest_doskonala/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace N_jest_doskonala
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program znajdzie dla ciebie liczby doskonale!");
+            Console.WriteLine("Podaj przedział od <1,n>");
+            Console.Write("Podaj N: ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int suma = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        suma = suma + j;
+                    }
+                }
+                if (suma == i)
+                    Console.WriteLine("Liczba {0} jest liczbą doskonałą", i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
